9fcc0f33bdb79329daa989bb3eb31faaca1a10fb
BrunoVieira005 <brunorv2005@gmail.com>
2024-10-30 13:06:21 -0300

    Adição de campo QRCode e Status no Esquema dos Ativos

--- backend/models/Asset.js
+++ backend/models/Asset.js
@@ -5,6 +5,8 @@ const assetSchema = new mongoose.Schema({
   acquisitionDate: String,
   cost: Number,
   lifeExpectancy: Number,
+  status: { type: String, default: 'active' }, // Ex: 'active', 'maintenance'
+  qrCode: String, // Armazena o URL do QR Code
 });
 
 module.exports = mongoose.model('Asset', assetSchema);
